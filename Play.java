4621f530ca6121e8284644b784b23d700a094826
BT228 <svc_OKC_BT228@ad.osuokc.edu>
2018-10-03 18:28:36 -0500

    play

---
+++ Play.java
@@ -0,0 +1,18 @@
+import Checkers.*;
+import java.util.Scanner;
+
+public class Play{
+	public static void main(String[] args){
+		Board board = new Board)();
+		Piece r1 = new Piece();
+		Piece b1 = new Piece();
+		
+		r1.set(Piece.color.RED);
+		b1.set(Piece.color.BLACK);
+		board.setPiece(b1,0,0);
+		board.setPiece(r1,7,7);
+		
+		System.out.println("Location 2x2 is occupied " + board.checkOccupied(2,2));
+		System.out.println("Location 7x7 is occupied " + board.checkOccupied(7,7));
+	}
+}
