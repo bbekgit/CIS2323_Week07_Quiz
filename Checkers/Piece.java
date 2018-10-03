4decc6396789e4646934bb6312ca83f4e4963336
BT228 <svc_OKC_BT228@ad.osuokc.edu>
2018-10-03 18:11:53 -0500

    Piece

---
+++ Checkers/Piece.java
@@ -0,0 +1,25 @@
+package Checkers;
+
+public class Piece{
+	
+	//Class Attributes
+	enum Color{RED,BLACK};
+	private static int numberOPieces = 0;
+	
+	//Instance Attributes
+	private Color color;
+	private int id;
+	
+	public piece(){
+		id=numberOPieces;
+		numberOPieces++;
+	}
+	
+	public void set(Color c){
+		color = c;
+	}
+	
+	//public int pieceCount(){
+		//return numberOPieces;
+	//} 
+}
